b6c1e6371f697a895ad3b7ad9c30017da4fc3abb
raivo.lapins <lapins.raivo@gmail.com>
2020-05-18 09:38:08 +0300

    Functioning index page

---
+++ src/main/java/com/slgproduction/mealapp/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.slgproduction.mealapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("${application.context}/${application.version}")
+public class IndexController {
+
+    @GetMapping("/")
+    public ModelAndView getIndex() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
+
+}
