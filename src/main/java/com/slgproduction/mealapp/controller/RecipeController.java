41ee6716e7768d17770a330bf5e666f581e821f5
raivo.lapins <raivo.lapins@accenture.com>
2020-05-26 18:42:24 +0300

    Not working

--- src/main/java/com/slgproduction/mealapp/controller/RecipeController.java
+++ src/main/java/com/slgproduction/mealapp/controller/RecipeController.java
@@ -1,16 +1,12 @@
 package com.slgproduction.mealapp.controller;
 
-import com.slgproduction.mealapp.model.Ingredient;
-import com.slgproduction.mealapp.model.Recipe;
-import com.slgproduction.mealapp.service.IngredientService;
-import com.slgproduction.mealapp.service.RecipeService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import com.slgproduction.mealapp.model.*;
+import com.slgproduction.mealapp.service.*;
+import lombok.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,12 +15,17 @@ public class RecipeController {
 
     private final RecipeService recipeService;
 
-    @GetMapping("/recipe/new")
+    @GetMapping("/recipe")
     public ModelAndView createNewRecipe() {
         ModelAndView modelAndView = new ModelAndView("addRecipe");
         Recipe recipe = recipeService.createNewRecipe();
         modelAndView.addObject("recipe", recipe);
         return modelAndView;
+    }
+
+    @PostMapping("/recipe")
+    public void saveRecipe() {
+
     }
 
     // Mapping to home.html for testing purposes
