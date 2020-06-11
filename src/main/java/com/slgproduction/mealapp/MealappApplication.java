e7ecd11ca9aa902c77efe3b78252f6602bebb4f8
Raivo Lapins <lapins.raivo@gmail.com>
2020-06-11 19:03:20 +0300

    AWS stuff

--- src/main/java/com/slgproduction/mealapp/MealappApplication.java
+++ src/main/java/com/slgproduction/mealapp/MealappApplication.java
@@ -3,13 +3,22 @@ package com.slgproduction.mealapp;
 import com.slgproduction.mealapp.model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MealappApplication {
+public class MealappApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MealappApplication.class, args);
+	}
+
+	/**
+	 * This method is used for war file build
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(MealappApplication.class);
 	}
 
 }
