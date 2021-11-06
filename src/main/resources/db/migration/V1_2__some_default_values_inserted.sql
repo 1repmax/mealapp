317542e98df362965e37c072cd2f66e786d30a3d
Aigars Strods <strods.aigars@gmail.com>
2021-11-06 15:59:10 +0200

    Docker mysql database

--- src/main/resources/db/migration/V1_2__some_default_values_inserted.sql
+++ src/main/resources/db/migration/V1_2__some_default_values_inserted.sql
@@ -1,14 +1,14 @@
 
-INSERT INTO products (name) VALUES ("milti");
-INSERT INTO products (name) VALUES ("aboli");
-INSERT INTO products (name) VALUES ("olas");
-INSERT INTO products (name) VALUES ("cukurs");
+INSERT INTO products (name) VALUES ("flour");
+INSERT INTO products (name) VALUES ("apples");
+INSERT INTO products (name) VALUES ("eggs");
+INSERT INTO products (name) VALUES ("sugar");
 
-INSERT INTO units (name, denotation) VALUES ("grami", "g");
-INSERT INTO units (name, denotation) VALUES ("gabali", "gab");
+INSERT INTO units (name, denotation) VALUES ("grams", "g");
+INSERT INTO units (name, denotation) VALUES ("pieces", "pcs");
 
-INSERT INTO recipes (name, short_description, cooking_time, usage_frequency, servings_per_recipe) VALUES ("Pīrāgs", "Labākais pīrāgs pasaulē" ,3600, 1, 4);
-INSERT INTO recipes (name, short_description, cooking_time, usage_frequency, servings_per_recipe) VALUES ("Omlete", "Viegla recepte iesācējiem", 600, 1, 1);
+INSERT INTO recipes (name, short_description, cooking_time, usage_frequency, servings_per_recipe) VALUES ("Pie", "Worlds best pie" ,3600, 1, 4);
+INSERT INTO recipes (name, short_description, cooking_time, usage_frequency, servings_per_recipe) VALUES ("Omlet", "Easy to cook for beginners", 600, 1, 1);
 
 -- Recipe for apple pie
 INSERT INTO ingredients (product_id, quantity, unit_id, recipe_id) VALUES (1, 300, 1, 1);
@@ -16,12 +16,12 @@ INSERT INTO ingredients (product_id, quantity, unit_id, recipe_id) VALUES (2, 2,
 INSERT INTO ingredients (product_id, quantity, unit_id, recipe_id) VALUES (3, 3, 2, 1);
 INSERT INTO ingredients (product_id, quantity, unit_id, recipe_id) VALUES (4, 100, 1, 1);
 
-INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (1, "No olām un miltiem sajauc mīklu", 1);
-INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (2, "Mīklu liek veidnē un pārklāj ar āboliem un cukuru", 1);
-INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (3, "Liek cepties cepeškrāsnī 200 grādos", 1);
+INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (1, "Make butter from eggs and flour", 1);
+INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (2, "Spread butter in baking pan and add apples and sugar", 1);
+INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (3, "Put in the oven bake at 200 degrees", 1);
 
 -- Recipe for omlet
 INSERT INTO ingredients (product_id, quantity, unit_id, recipe_id) VALUES (3, 2, 2, 2);
 
-INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (1, "Pārsit olas un sakuļ", 2);
-INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (2, "Cepj sviestā uz pannas", 2);
+INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (1, "Brake eggs and whisk eggs", 2);
+INSERT INTO cooking_steps (step_number, description, recipe_id) VALUES (2, "Bake in butter on the pan", 2);
